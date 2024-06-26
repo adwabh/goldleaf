98f51c83f1b3dd0c9313e2f8c0d58cb3a3a3dea0
Adwait Abhyankar <avabhyankar22@gmail.com>
2024-06-27 02:06:05 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,36 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { initFlowbite } from 'flowbite';
+import { faSun, faMoon, faChartLine, faBullseye, faMobileAlt } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { AsyncPipe, NgFor, NgIf, NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+    FontAwesomeModule,
+    NgIf,
+    AsyncPipe,
+    NgClass],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'landingpage';
+  isDarkMode = false;
+  faSun = faSun;
+  faMoon = faMoon;
+  faChartLine = faChartLine;
+  faBullseye = faBullseye;
+  faMobileAlt = faMobileAlt;
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
+
+  toggleTheme() {
+    this.isDarkMode = !this.isDarkMode;
+    document.body.classList.toggle('dark');
+  }
 }
